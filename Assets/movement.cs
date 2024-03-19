a32d2296093e7f914dfd5e7d9e59368005c0dfe2
AchoDev <ahmedhdasi89@gmail.com>
2024-03-19 08:00:34 +0100

    walking && running

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -8,9 +8,13 @@ public class movement : MonoBehaviour
     Animator animator;
     Rigidbody2D rb;
 
-    [SerializeField] float speed = 10.0f;
+    [SerializeField] float speed;
+    [SerializeField] float sprintSpeed;
+
+    bool sprinting = false;
 
     float moveDirection = 0;
+    float currentRotation = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -23,16 +27,25 @@ public class movement : MonoBehaviour
     void Update()
     {
         moveDirection = Input.GetAxis("Horizontal");
+
+        sprinting = Input.GetKey(KeyCode.LeftShift);
+        animator.SetBool("sprinting", sprinting);
     }
 
     void FixedUpdate() 
     {
-        rb.velocity = new Vector2(moveDirection, rb.velocity.y) * speed;
+        rb.velocity = new Vector2(moveDirection * speed * (sprinting ? sprintSpeed : 1), rb.velocity.y);
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation, 
+            Quaternion.Euler(0, currentRotation, 0), 
+            0.15f
+        );
 
         if(Input.GetAxisRaw("Horizontal") != 0)
         {
+            currentRotation = moveDirection > 0 ? 0 : 180;
             animator.SetBool("running", true);
-            transform.rotation = moveDirection > 0 ? Quaternion.Euler(0, 0, 0) : Quaternion.Euler(0, 180, 0);
         } else 
         {
             animator.SetBool("running", false);
