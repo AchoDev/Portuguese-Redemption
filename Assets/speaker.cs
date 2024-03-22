a541ead82fea667b31beabb6124dd3f2d6dfedc1
AchoDev <ahmedhdasi89@gmail.com>
2024-03-22 11:40:59 +0100

    dialogue system improved in various ways + new bick dialogue

--- Assets/speaker.cs
+++ Assets/speaker.cs
@@ -1,22 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEditor.PackageManager.Requests;
+using UnityEditor.Search;
+using UnityEditor.UI;
 using UnityEngine;
 
 enum People 
 {  
-    Leo,
+    LeoSuit,
     BickNuterus,
-}
-
-enum Clothing 
-{
-    normal,
-    suit,
 }
 
 enum Emotion {
@@ -32,7 +30,6 @@ class Message {
     public People name;
     [TextArea(3, 10)]
     public string dialogue;
-    public Clothing clothing = Clothing.normal;
     public Emotion emotion = Emotion.neutral;
 }
 
@@ -41,10 +38,20 @@ public class speaker : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    [Space(10)]
+    [Header("General")]
+    [SerializeField] bool turnTowardsPlayer = false;
+    [SerializeField] bool setAnimatorTalking = false;
     [SerializeField, Range(0, 0.1f)] float timeBetweenLetters = 0.025f;
+    
+    [Space(10)]
+    [Header("Camera")]
+    [SerializeField] bool focusCamera = true;
     [SerializeField, Range(0.1f, 5f)] float cameraSpeed = 1f;
     [SerializeField, Range(0.1f, 5)] float ortho = 2.5f;
+
     [Space(10)]
+    [Header("Dialogue")]
     [SerializeField] Message[] dialogue;
 
 
@@ -57,12 +64,19 @@ public class speaker : MonoBehaviour
     movement playerMovement;
     GameObject eIndicator;
 
+    Vector3 turnVelocity = Vector3.zero;
+    float originalSize;
+
     bool playerInTrigger = false;
     bool talking = false;
 
     int currentLine = 0;
     int currentIndex = 0;
     string currentText = "";
+
+    string SplitCamelCase(string source) {
+        return Regex.Split(source, @"(?<!^)(?=[A-Z])").Aggregate((a, b) => a + " " + b);
+    }
 
     void Start()
     {
@@ -78,6 +92,7 @@ public class speaker : MonoBehaviour
         cameraFocusPoint.cameraSpeed = cameraSpeed;
         cameraFocusPoint.ortho = ortho;
 
+        originalSize = transform.localScale.x;
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -95,6 +110,7 @@ public class speaker : MonoBehaviour
         {
             Debug.Log("Player exited the trigger");
             playerInTrigger = false;
+            eIndicator.SetActive(false);
         }
     }
 
@@ -110,7 +126,10 @@ public class speaker : MonoBehaviour
 
         playerMovement.allowMovement = false;
 
-        
+        if(setAnimatorTalking) {
+            GetComponent<Animator>().SetBool("talking", true);
+        }
+
         cameraFocusPoint.Focus(
             (transform.position + playerMovement.transform.position) / 2
         );
@@ -119,19 +138,50 @@ public class speaker : MonoBehaviour
         StartCoroutine(finishAnimation());
     }
 
+    void turnAround() 
+    {
+        if(!turnTowardsPlayer) return;
+        if(talking) {
+            float newSize = playerMovement.transform.position.x - transform.position.x > 0 ? originalSize : -originalSize;
+
+            transform.localScale = Vector3.SmoothDamp(
+                transform.localScale, 
+                new Vector3(newSize, transform.localScale.y, transform.localScale.z), 
+                ref turnVelocity,
+                0.1f
+            );
+        } else {
+            transform.localScale = Vector3.SmoothDamp(
+                transform.localScale, 
+                new Vector3(originalSize, transform.localScale.y, transform.localScale.z), 
+                ref turnVelocity,
+                0.1f
+            );
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(talking) textBox.text = currentText;
-        eIndicator.SetActive(playerInTrigger && !talking);
-        
-        if(!(playerInTrigger && Input.GetKeyDown(KeyCode.E))) return;
-        
-        
-        if(!talking) {
-            initiateTalk();
+
+        if(playerInTrigger && !talking) eIndicator.SetActive(true);
+
+        turnAround();
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(!talking)
+            {
+                if(playerInTrigger && playerMovement.allowMovement) {
+                    initiateTalk();
+                }
+                return;
+            }
+        } else {
             return;
-        } 
+        }
+        
         
         string text = dialogue[currentLine].dialogue;
         if(currentIndex < text.Length) // skip to end of line
@@ -147,34 +197,47 @@ public class speaker : MonoBehaviour
                 currentLine = 0;
                 currentText = "";
                 animator.SetBool("speaking", false);
-                playerMovement.allowMovement = true;
+                // playerMovement.allowMovement = true;
                 cameraFocusPoint.Unfocus();
+
+                StartCoroutine(waitBeforeEnd(dialogue[dialogue.Length - 1].name.ToString()));
+
+                if(setAnimatorTalking) {
+                    GetComponent<Animator>().SetBool("talking", false);
+                }
             } else 
             {
-                
+                disableCurrentImages();
                 setNewImage();
                 StartCoroutine(talk());
             }
         }
     }
 
+    void disableCurrentImages()
+    {
+        GameObject parent = animator.transform.Find($"Image/{dialogue[currentLine - 1].name.ToString()}").gameObject;
+        for(int i = 0; i < parent.transform.childCount; i++)
+        {
+            parent.transform.GetChild(i).gameObject.SetActive(false);
+        }   
+    }
+
     void setNewImage()
     {
         Message message = dialogue[currentLine];
 
-        nameBox.text = message.name.ToString();
+        nameBox.text = SplitCamelCase(message.name.ToString());
 
         GameObject parent = animator.transform.Find($"Image/{message.name.ToString()}").gameObject;
-        for(int i = 0; i < parent.transform.childCount; i++)
-        {
-            parent.transform.GetChild(i).gameObject.SetActive(false);
-        }
 
-        string searchingFor = $"{message.clothing.ToString()}-{message.emotion.ToString()}";
+        string searchingFor = message.emotion.ToString();
 
-        if(message.clothing == Clothing.normal)
-        {
-            searchingFor = message.emotion.ToString();
+        Debug.Log(parent);
+
+        if(parent.transform.Find(searchingFor) == null) {
+            Debug.LogWarning("No image found for emotion: " + searchingFor + " in " + message.name.ToString() + "!");
+            return;
         }
 
         parent.transform.Find(searchingFor).gameObject.SetActive(true);
@@ -196,6 +259,19 @@ public class speaker : MonoBehaviour
     {
         yield return new WaitForSeconds(0.4f);
         StartCoroutine(talk());
+    }
+
+    IEnumerator waitBeforeEnd(string lastSpeaker)
+    {
+
+        yield return new WaitForSeconds(0.4f);
+        playerMovement.allowMovement = true;
+        GameObject parent = animator.transform.Find($"Image/{lastSpeaker}").gameObject;
+        for(int i = 0; i < parent.transform.childCount; i++)
+        {
+            parent.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
     }
 
     void Reset()
