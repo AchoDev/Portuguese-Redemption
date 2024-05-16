947ae0e1f8e2e94629ed63b0cc4b4fe34479a377
AchoDev <ahmedhdasi89@gmail.com>
2024-05-16 09:21:35 +0200

    wingsuit physics

--- Assets/FlightController.cs
+++ Assets/FlightController.cs
@@ -7,23 +7,49 @@ public class FlightController : MonoBehaviour
 {
 
     Animator animator;
+    Rigidbody2D rb;
     int flightDirection = 0;
+
+    [SerializeField] float liftForce = 100f;
+    [SerializeField] float speedForce = 100f;
+    [SerializeField] float diveMultiplier = 4f;
+    [SerializeField, Range(0, 0.1f)] float horizontalSpeedMultiplier = 4f;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(10 * Vector2.right, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        flightDirection = (int)Input.GetAxisRaw("Vertical");
+        flightDirection = -(int)Input.GetAxisRaw("Vertical");
         animator.SetInteger("flightDirection", flightDirection);
     }
 
     void FixedUpdate() 
     {
-        transform.position += new Vector3(0.1f, flightDirection * 0.1f, 0);
+        float gravity = -9.81f;
+
+        float xForce = Mathf.Max(0, -rb.velocity.y * speedForce);
+        float yForce = liftForce * (flightDirection + 1) * (rb.velocity.x * horizontalSpeedMultiplier);
+
+        if(flightDirection == 1)
+        {
+            yForce *= diveMultiplier;
+            xForce /= diveMultiplier;
+        } else if(flightDirection == -1)
+        {
+            yForce *= diveMultiplier;
+            // xForce *= diveMultiplier;
+        }
+
+        yForce += gravity;
+
+        rb.AddForce(new Vector2(xForce, yForce));
     }
 }
